1154efe087fe9e2c1450f1352c16364cf7abd21b
AleksandrKoryakin <88359979+AleksandrKoryakin@users.noreply.github.com>
2022-04-27 10:08:22 +0300

    Add files via upload

--- js/accordeon.js
+++ js/accordeon.js
@@ -1,16 +1,41 @@
-// массив заголовков
-const titles = document.querySelectorAll('.program-line__title')
-// массив описаний
-const descrs = document.querySelectorAll('.program-line__descr')
+// // массив заголовков
+// const titles = document.querySelectorAll('.program-line__title')
+// // массив описаний
+// const descrs = document.querySelectorAll('.program-line__descr')
 
-// на каждый заголовок вешаем событие + запоминаем индекс заголовка
-titles.forEach((title, index) => {
-  title.addEventListener('click', () => {
-    // каждое описание выключаем
-    descrs.forEach((descr) => {
-      descr.classList.remove('active');
+// // на каждый заголовок вешаем событие + запоминаем индекс заголовка
+// titles.forEach((title, index) => {
+//   title.addEventListener('click', () => {
+//     // каждое описание выключаем
+//     descrs.forEach((descr) => {
+//       descr.classList.remove('active');
+//     })
+//     // индексированное описание включаем
+//     descrs[index].classList.add('active');
+//   })
+// })
+
+const accordeon = () => {
+  const contents = document.querySelectorAll('.program-line__content')
+  const contentsDescr = document.querySelectorAll('.program-line__descr')
+
+  contents.forEach((elem) => {
+    const title = elem.querySelector('.program-line__title')
+    const descr = elem.querySelector('.program-line__descr')
+
+    descr.style.transition = 'height .3s'
+    descr.style.overflow = 'hidden'
+
+    title.addEventListener('click', () => {
+      contentsDescr.forEach((tab) => {
+        if (tab === descr) {
+          tab.style.height = tab.scrollHeight + 'px'
+        } else {
+          tab.style.height = 0
+        }
+      })
     })
-    // индексированное описание включаем
-    descrs[index].classList.add('active');
   })
-})
+}
+
+accordeon()
